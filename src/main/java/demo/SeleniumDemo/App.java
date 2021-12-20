d8e64976c8c960f405ac897472e23baeb9f757e6
vishnuraj-git <vishnuraj391@gmail.com>
2021-12-20 22:33:23 +0530

    first commit

---
+++ src/main/java/demo/SeleniumDemo/App.java
@@ -0,0 +1,29 @@
+package demo.SeleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.co.in");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("flowers");
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys(Keys.RETURN);
+       // driver.findElement(By.xpath("//input[@name='q']")).sendKeys(Keys.ENTER);
+			
+		
+    }
+}
